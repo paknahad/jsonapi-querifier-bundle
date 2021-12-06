9fe5d20a71a79665be89d0ac7db98f7dcc19a6a6
Hamid Paknahad <hp.paknahad@gmail.com>
2021-12-07 01:42:54 +0330

    update bundle extension for Symfony 6

--- src/JsonApiQuerifierBundle.php
+++ src/JsonApiQuerifierBundle.php
@@ -3,11 +3,12 @@
 namespace Paknahad\JsonApiQuerifierBundle;
 
 use Paknahad\JsonApiQuerifierBundle\DependencyInjection\JsonApiQuerifierExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class JsonApiQuerifierBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new JsonApiQuerifierExtension();
     }
